457e9c9378bf588b643fb564bd6c28b84837ef58
Tony <mao9085@outlook.com>
2022-04-07 10:13:09 +0800

    简化 for 循环代码，简化结构体赋值语法

--- dataStructures/sparsearray.go
+++ dataStructures/sparsearray.go
@@ -31,16 +31,15 @@ func TwoDimensionalArray() [11][11]int {
 	var chessBoard [11][11]int
 	chessBoard[1][2] = 1
 	chessBoard[2][3] = 1
-
 	fmt.Println("↓原始数据↓")
 	for _, v := range chessBoard {
 		for _, v2 := range v {
+			// %v 相应值的默认格式，格式化输出
 			fmt.Printf("%v\t", v2)
 		}
 		fmt.Println()
 	}
 	return chessBoard
-
 }
 
 //  二维数组转稀疏数组
@@ -61,20 +60,35 @@ func ArrayToSparsearry(chessBoard [11][11]int) []DataNode {
 	dataNodes = append(dataNodes, dataNode)
 	dataNodes[0] = dataNode
 
-	for i := 0; i < len1; i++ {
-		for j := 0; j < len2; j++ {
+	for i, v1 := range chessBoard {
+		for j, v2 := range v1 {
+			// 找出二维矩阵中不为 0 的数值，并将其添加到稀疏数组中
 			if chessBoard[i][j] != 0 {
-				dataNode := DataNode{
-					row: i,
-					col: j,
-					val: chessBoard[i][j],
-				}
-				count++
+				// 结构体赋值
+				dataNode := DataNode{i, j, v2}
+				// 添加棋盘棋子信息到 稀疏数组中
 				dataNodes = append(dataNodes, dataNode)
+				// 为稀疏数组第一行的 value 值计数
+				count++
 			}
 		}
-
 	}
+
+	//for i := 0; i < len1; i++ {
+	//	for j := 0; j < len2; j++ {
+	//		// 找出二维矩阵中不为 0 的数值，并将其添加到稀疏数组中
+	//		if chessBoard[i][j] != 0 {
+	//			// 构建稀疏数组某行的值 dataNode
+	//			dataNode := DataNode{i,j,chessBoard[i][j]}
+	//			// 添加棋盘棋子信息到 稀疏数组中
+	//			dataNodes = append(dataNodes, dataNode)
+	//			// 为稀疏数组第一行的 value 值计数
+	//			count++
+	//		}
+	//	}
+	//
+	//}
+
 	dataNodes[0].val = count
 
 	return dataNodes
